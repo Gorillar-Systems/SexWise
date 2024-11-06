c626e062973c68cbd429e6de98467b6f66267b2d
governor <ron468896@gmail.com>
2024-11-06 22:16:35 +0000

    Start Navbar

---
+++ src/constants/index.js
@@ -0,0 +1,22 @@
+export const NAVLINKS = [
+  {
+    link: "Blog",
+    path: "/blog",
+  },
+  {
+    link: "About",
+    path: "/about",
+  },
+  {
+    link: "Contact",
+    path: "/contact",
+  },
+  {
+    link: "Advice",
+    path: "/advice",
+  },
+  {
+    link: "Therapist Jobs",
+    path: "/therapist-jobs",
+  },
+];
